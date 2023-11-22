0288078d5e7a214b0830d3ef66108db8569b945a
Gonzalo Fernandez <gondev85@gmail.com>
2023-11-21 21:18:14 -0300

    defining navbar style

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+export function Navbar() {
+    return (
+        <nav className="navbar">
+            <h1>Otherness Merceria</h1>
+            <ul>
+                <li>Catalogo</li>
+                <li>Iniciar Sesion</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+        
+    )
+}
